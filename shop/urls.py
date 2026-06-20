345741b5eea57438afba088c1ea8c841ca5abafc
IanChoDK <ian.ibanez@itecriocuarto.org.ar>
2026-06-19 23:54:39 -0300

    crear ropa agregada

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ropa_list, name='ropa_list'),
+    path("crear", views.crear_ropa, name="crear_ropa"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Ropa
+from .forms import RopaForm
 
 # Create your views here.
 
 def ropa_list(request):
     ropa = Ropa.objects.all()
-    return render(request, 'ropa_list.html', {'ropa': ropa})
+    return render(request, "ropa_list.html", {"ropa": ropa})
+
+def crear_ropa(request):
+    if request.method == "POST":
+        form = RopaForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("ropa_list")
+    else:
+        form = RopaForm()
+    return render(request, "crear_ropa.html", {"form": form})
