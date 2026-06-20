c4f2f7137c0901ce0adaeff087095490eba9f448
FrancoPereyra13 <f.pereyragonzalez@itecriocuarto.org.ar>
2026-06-19 22:06:02 -0300

    creacion de archivo models

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+
 class Ropa(models.Model):
     nombre = models.CharField(max_length=30)
     descripcion = models.TextField()
@@ -10,3 +12,4 @@ class Ropa(models.Model):
 
     def __str__(self):
         return f"{self.nombre} - ${self.precio}"
+    
