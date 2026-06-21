c1d8a79aa5f8e3e8ce57d2cfe0160bed14eeb145
IanChoDK <ian.ibanez@itecriocuarto.org.ar>
2026-06-21 14:06:41 -0300

    modelos en admin tienen filtros, ordenamiento y busqueda

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Ropa, Categoria
 
 # Register your models here.
-admin.site.register(Ropa)
-admin.site.register(Categoria)
+@admin.register(Ropa)
+class RopaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'categoria')
+    search_fields = ('nombre',)
+    list_filter = ('categoria',)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
